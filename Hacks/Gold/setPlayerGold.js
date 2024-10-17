284dbf0d22f3bc811e2c411e186e56638e2ec213
grey618 <29gsiemens@rrvsd.ca>
2024-10-17 14:46:26 -0500

    Update setPlayerGold.js

--- Hacks/Gold/setPlayerGold.js
+++ Hacks/Gold/setPlayerGold.js
@@ -3,7 +3,7 @@ function getStateNode(){
     for(let i of Object.keys(document.querySelector("#app>div>div"))){
         if(i.toString().includes("__reactEventHandlers")){
             for(let p of Object.values(document.querySelector("#app>div>div")[i].children.filter(n=>n))){
-                if(p._owner&&p._owner.stateNode)return p._owner.stateNode
+                if(p._owner&&p._owner.stateNode)return p._owner.stateNode{work no matter what
             }
         }
     }
